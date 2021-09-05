8202df0838ea10dc77221d41623900fe030ad6fd
Ryan Rieger <36425617+rmr6894@users.noreply.github.com>
2021-09-04 20:39:15 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -27,20 +27,20 @@ const getRandomJokeJSON = () => {
     const jokes = [
         ["What do you call a very small valentine?", "A valen-tiny!"],
         ["What did the dog say when he rubbed his tail on the sandpaper?", "Ruff, Ruff!"],
-		["Why don't sharks like to eat clowns?", "Because they taste funny!"],
-		["What did the boy cat say to the girl cat?", "You're Purr-fect!"],
-		["What is a frog's favorite outdoor sport?", "Fly Fishing!"],
-		["I hate jokes about German sausages.", "Theyre the wurst!"],
-		["Did you hear about the cheese factory that exploded in France?", "There was nothing left but de Brie!"],
-		["Our wedding was so beautiful ", "Even the cake was in tiers!"],
-		["Is this pool safe for diving?", "It deep ends!"],
-		["Dad, can you put my shoes on?", "I dont think theyll fit me!"],
-		["Can February March?", "No, but April May!"],
-		["What lies at the bottom of the ocean and twitches?", "A nervous wreck!"],
-		["Im reading a book on the history of glue.", "I just cant seem to put it down!"],
-		["Dad, can you put the cat out?", "I didnt know it was on fire!"],
-		["What did the ocean say to the sailboat?", "Nothing, it just waved!"],
-		["What do you get when you cross a snowman with a vampire?", "Frostbite!"]
+	["Why don't sharks like to eat clowns?", "Because they taste funny!"],
+	["What did the boy cat say to the girl cat?", "You're Purr-fect!"],
+	["What is a frog's favorite outdoor sport?", "Fly Fishing!"],
+	["I hate jokes about German sausages.", "Theyre the wurst!"],
+	["Did you hear about the cheese factory that exploded in France?", "There was nothing left but de Brie!"],
+	["Our wedding was so beautiful ", "Even the cake was in tiers!"],
+	["Is this pool safe for diving?", "It deep ends!"],
+	["Dad, can you put my shoes on?", "I dont think theyll fit me!"],
+	["Can February March?", "No, but April May!"],
+	["What lies at the bottom of the ocean and twitches?", "A nervous wreck!"],
+	["Im reading a book on the history of glue.", "I just cant seem to put it down!"],
+	["Dad, can you put the cat out?", "I didnt know it was on fire!"],
+	["What did the ocean say to the sailboat?", "Nothing, it just waved!"],
+	["What do you get when you cross a snowman with a vampire?", "Frostbite!"]
     ];
 
     const number = Math.floor(Math.random() * 16);
